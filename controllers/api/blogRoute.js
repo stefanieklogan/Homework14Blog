43eaef3a34ecb73d2904bf9ce130455cdd83c473
Stefanie L <75275831+stefanieklogan@users.noreply.github.com>
2021-03-28 13:20:30 -0400

    allBlogs handlebars

--- controllers/api/blogRoute.js
+++ controllers/api/blogRoute.js
@@ -1,31 +1,31 @@
-// const router = require('express').Router();
-// const { Blog } = require('../../models');
-// const withAuth = require('../../utils/auth');
+const router = require('express').Router();
+const { Blog } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 
-// router.get('/blog/:id', async (req, res) => {
-//   try {
-//   const blogData = await Blog.findByPk(req.params.id);
-//   console.log(blogData)
-//   const blog = blogData.get({ plain: true });
-//   res.render('blog', blog);
-//   } catch (err) {
-//       res.status(500).json(err);
-//   }
-// });
+router.get('/blog/:id', async (req, res) => {
+  try {
+  const blogData = await Blog.findByPk(req.params.id);
+  console.log(blogData)
+  const blog = blogData.get({ plain: true });
+  res.render('blog', blog);
+  } catch (err) {
+      res.status(500).json(err);
+  }
+});
 
-// router.post('/', withAuth, async (req, res) => {
-//   try {
-//     const newBlog = await Blog.create({
-//       ...req.body,
-//       user_id: req.session.user_id,
-//     });
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newBlog = await Blog.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
 
-//     res.status(200).json(newBlog);
-//   } catch (err) {
-//     res.status(400).json(err);
-//   }
-// });
+    res.status(200).json(newBlog);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 // router.delete('/:id', withAuth, async (req, res) => {
 //   try {
