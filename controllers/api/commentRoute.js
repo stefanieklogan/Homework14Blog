f1eeaa66ca9f147b2688633c3f408478371c1dab
Stefanie L <75275831+stefanieklogan@users.noreply.github.com>
2021-04-04 14:27:48 -0400

    updated comment and post routes

--- controllers/api/commentRoute.js
+++ controllers/api/commentRoute.js
@@ -1,38 +1,39 @@
-// const router = require('express').Router();
-// const { Comment } = require('../../models');
-// const withAuth = require('../../utils/auth');
+const router = require('express').Router();
+const { Comment } = require('../../models');
 
-// router.post('/', withAuth, async (req, res) => {
-//   try {
-//     const newComment = await Comment.create({
-//       ...req.body,
-//       user_id: req.session.user_id,
-//     });
+router.post('/', async (req, res) => {
+  try {
+    const newComment = {
+      comment: req.body.comment,
+      user_id: req.session.user_id,
+      post_id: req.body.post_id
+    }
+    const comment = await Comment.create(newComment);
 
-//     res.status(200).json(newComment);
-//   } catch (err) {
-//     res.status(400).json(err);
-//   }
-// });
+    res.status(200).json(comment);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
-// router.delete('/:id', withAuth, async (req, res) => {
-//   try {
-//     const commentData = await Comment.destroy({
-//       where: {
-//         id: req.params.id,
-//         user_id: req.session.user_id,
-//       },
-//     });
+router.delete('/:id', async (req, res) => {
+  try {
+    const commentData = await Comment.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
 
-//     if (!commentData) {
-//       res.status(404).json({ message: 'No comment found with this id!' });
-//       return;
-//     }
+    if (!commentData) {
+      res.status(404).json({ message: 'No comments found with this id!' });
+      return;
+    }
 
-//     res.status(200).json(commentData);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-// module.exports = router;
+module.exports = router;
---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const { Post } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+
+router.post('/', async(req, res) => {
+  if (!req.session.logged_in) {
+    res.status(400).json({ logged_in: false, message: 'Please log in first' });
+    return;
+  }
+
+  try {
+    let newPost = {
+      title: req.body.title,
+      content: req.body.content,
+      user_id: req.session.user_id,
+    }
+    let post = await Post.create(newPost);
+    res.status(200).json(post);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+});
+
+module.exports = router;
