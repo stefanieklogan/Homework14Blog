6e342cd122458a973f97870f1bbdb754b4d17d65
Stefanie L <75275831+stefanieklogan@users.noreply.github.com>
2021-04-04 14:23:26 -0400

    udpdated post seed with user_id

--- seeds/postData.js
+++ seeds/postData.js
@@ -3,11 +3,13 @@ const { Post } = require('../models');
 const postData = [
   {
     "title": "MS Access, can you hear me?",
-    "content": "It's been a good ride, but many developers have said goodbye to Microsoft Access. What do you think? Gone too soon?"
+    "content": "It's been a good ride, but many developers have said goodbye to Microsoft Access. What do you think? Gone too soon?",
+    "user_id": 1
   },
   {
       "title": "Who's the best web developer?",
-      "content": "We're here to find out - comment below!"
+      "content": "We're here to find out - comment below!",
+      "user_id": 1
     }
 ];
 
