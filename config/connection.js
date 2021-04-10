45b81300a9f3de45696afc5d25c1df2efbd13a2f
Stefanie L <75275831+stefanieklogan@users.noreply.github.com>
2021-04-10 16:32:38 -0400

    updated connectionjs

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
-require('dotenv').config();
 const Sequelize = require('sequelize');
+require('dotenv').config();
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+const sequelize = new Sequelize(process.env.JAWSDB_URL);
+
 module.exports = sequelize;
+
+
+// const sequelize = process.env.JAWSDB_URL
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       dialectOptions: {
+//         decimalNumbers: true,
+//       },
+//     });
+// module.exports = sequelize;
